771d9dcf10233e07f52534418b4f94572532ee2e
Gilbert <87858323+gfgafn@users.noreply.github.com>
2022-07-09 17:44:38 +0800

    feat: Complete the output and shutdown based on the RustSBI

--- os/src/main.rs
+++ os/src/main.rs
@@ -1,7 +1,28 @@
 #![no_std]
 #![no_main]
+#![feature(panic_info_message)]
+
+#[macro_use]
+mod console;
 
 mod lang_items;
+mod sbi;
 
 use core::arch::global_asm;
+
 global_asm!(include_str!("entry.asm"));
+
+#[no_mangle]
+pub fn rust_main() -> ! {
+    clear_bss();
+    println!("Hello, world!");
+    panic!("Shutdown machine!");
+}
+
+fn clear_bss() {
+    extern "C" {
+        fn sbss();
+        fn ebss();
+    }
+    (sbss as usize..ebss as usize).for_each(|a| unsafe { (a as *mut u8).write_volatile(0) })
+}
