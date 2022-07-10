62e1ffe01e5b46072233b32208387dff84da9d70
Gilbert <87858323+gfgafn@users.noreply.github.com>
2022-07-10 20:03:18 +0800

    feat(Lab1/log): log with different color

--- os/src/console.rs
+++ os/src/console.rs
@@ -29,3 +29,38 @@ macro_rules! println {
         $crate::console::print(format_args!(concat!($fmt, "\n") $(, $($arg)+)?));
     }
 }
+
+#[macro_export]
+macro_rules! error {
+    ($fmt: literal $(, $($arg: tt)+)?) => {
+        $crate::console::print(format_args!(concat!("\x1b[31m[ERROR] ",$fmt,"\x1b[0m", "\n") $(, $($arg)+)?));
+    }
+}
+
+#[macro_export]
+macro_rules! warn {
+    ($fmt: literal $(, $($arg: tt)+)?) => {
+        $crate::console::print(format_args!(concat!("\x1b[93m[WARN] ",$fmt,"\x1b[0m", "\n") $(, $($arg)+)?));
+    }
+}
+
+#[macro_export]
+macro_rules! info {
+    ($fmt: literal $(, $($arg: tt)+)?) => {
+        $crate::console::print(format_args!(concat!("\x1b[34m[INFO] ",$fmt,"\x1b[0m", "\n") $(, $($arg)+)?));
+    }
+}
+
+#[macro_export]
+macro_rules! debug {
+    ($fmt: literal $(, $($arg: tt)+)?) => {
+        $crate::console::print(format_args!(concat!("\x1b[32m[DEBUG] ",$fmt,"\x1b[0m", "\n") $(, $($arg)+)?));
+    }
+}
+
+#[macro_export]
+macro_rules! trace {
+    ($fmt: literal $(, $($arg: tt)+)?) => {
+        $crate::console::print(format_args!(concat!("\x1b[90m[TRACE] ",$fmt,"\x1b[0m", "\n") $(, $($arg)+)?));
+    }
+}
--- os/src/main.rs
+++ os/src/main.rs
@@ -12,17 +12,38 @@ use core::arch::global_asm;
 
 global_asm!(include_str!("entry.asm"));
 
+extern "C" {
+    fn stext();
+    fn etext();
+    fn srodata();
+    fn erodata();
+    fn sdata();
+    fn edata();
+    fn sbss();
+    fn ebss();
+    fn _start();
+    fn boot_stack();
+    fn boot_stack_top();
+}
+
 #[no_mangle]
 pub fn rust_main() -> ! {
     clear_bss();
-    println!("Hello, world!");
+    info!("load range: _start = {:#x}", _start as usize);
+    info!(
+        "boot_stack [{:#x}, {:#x}]",
+        boot_stack as usize, boot_stack_top as usize
+    );
+    info!(".text [{:#x}, {:#x}]", stext as usize, etext as usize);
+    info!(".rodata [{:#x}, {:#x}]", srodata as usize, erodata as usize);
+    info!(".data [{:#x}, {:#x}]", sdata as usize, edata as usize);
+    error!("Hello, world!");
+    warn!("This is warnning message!");
+    trace!("This is trace message!");
+    debug!("This is debug message!");
     panic!("Shutdown machine!");
 }
 
 fn clear_bss() {
-    extern "C" {
-        fn sbss();
-        fn ebss();
-    }
     (sbss as usize..ebss as usize).for_each(|a| unsafe { (a as *mut u8).write_volatile(0) })
 }
